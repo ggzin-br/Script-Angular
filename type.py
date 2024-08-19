4e954694e2cbe6574609b67ff1d78cdbc951ef3f
Pedro Garcia Vilanova <pedrovilanova2@hotmail.com>
2024-08-19 18:52:28 -0300

    Alteração para praticidade do usuário e boas práticas de código

--- type.py
+++ type.py
@@ -6,15 +6,19 @@ import os
 
 class Type(Padrao):
 
+    ## Declaração de variáveis #
+    typeenv: str
+    tscconfig: str
+    tasksconfig: str
 
-    def __init__(self, typeenv: str, tscconfig: str, tasksconfig):
-        self.typeenv = os.path.join(os.path.expanduser("~"), typeenv)
+    def __init__(self, typeenv: str, tscconfig: str, tasksconfig: str):
+        self.typeenv = typeenv
         self.tscconfig = tscconfig
         self.tasksconfig = tasksconfig
 
     def rodar(self):
         
-        ## Criar o diretório e os arquivos para o Type
+        ## Criar o diretório e os arquivos para o Type #
         try:
             os.mkdir(self.typeenv) ## Dir do diretório padrão
             os.mkdir(os.path.join(self.typeenv, ".vscode")) 
@@ -22,7 +26,7 @@ class Type(Padrao):
         except FileExistsError:
             print("O diretório já existe, prosseguindo com a instalação...: \n")
 
-        ## Vscode e configs
+        ## Vscode e configs #
         with open(os.path.join(self.typeenv, "tsconfig.json"), "+w") as arq: # já da o .close()
             arq.write(self.tscconfig)
         with open(os.path.join(self.typeenv, ".vscode", "tasks.json"), "w+") as arq:
